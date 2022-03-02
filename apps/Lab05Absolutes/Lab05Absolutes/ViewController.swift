8c95f5e18c27526361417278331076fa235829f3
Jordan Ganaway <jordanganaway@gmail.com>
2022-03-02 17:46:52 -0600

    Project 5

---
+++ apps/Lab05Absolutes/Lab05Absolutes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Lab05Absolutes
+//
+//  Created by Jordan Ganaway on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        firstView.addSubview(secondView)
+    }
+}
